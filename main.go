70dec0e3d680fedc8c88ed8c722131464ce8a737
Vladimir Pershukov <vladimirpershukov@yandex.ru>
2020-01-10 17:04:24 +0300

    assignments 1-3

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 
 var (
 	seed = flag.Int("seed", 0, "seed for random generator. unix(now) be default")
+	start = flag.Int("start", 1, "Additional param start for random generator.")
+	end = flag.Int("end", 6, "Additional param end for random generator.")
+	n = flag.Int("n", 1, "Additional param n for random generator cycle.")
 )
 
 // Фукнция должна вернуть число из интервала [l,r]
-//func randInterval
-//	return 0
-//}
+func randInterval(min, max int) int {
+	return rand.Intn(max - min + 1) + min
+}
 
 
 func main() {
@@ -25,5 +28,15 @@ func main() {
 		rand.Seed(int64(*seed))
 	}
 	// Dice roll 1..6
-	fmt.Println(rand.Intn(6) + 1)
+	fmt.Println(randInterval(1,6))
+
+	if *start > *end{
+		fmt.Println("Error: program stopped")
+	} else {
+		fmt.Println(randInterval(*start, *end))
+	}
+
+	for i := 0; i < *n; i++ {
+		fmt.Println(randInterval(*start, *end))
+	}
 }
